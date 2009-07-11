8a73ba3de88ad119113193a48f76eb0e092a420e
Jared Hirsch <spambait@jaredhirsch.com>
2009-07-11 10:33:17 -0400

    working out json-to-array parsing test. going to need a fake absolutelyCool class...

--- FrontControllerTest.php
+++ FrontControllerTest.php
@@ -2,6 +2,12 @@
 
 require_once 'simpletest/autorun.php';
 require_once 'FrontController.php';
+require_once 'AbsolutelyCool.php';
+
+class AbsolutelyCoolStub extends AbsolutelyCool
+{
+    
+}
 
 class FrontControllerTest extends UnitTestCase
 {
@@ -15,6 +21,22 @@ class FrontControllerTest extends UnitTestCase
 
     public function testShouldPassDecodedInputToCoolRunnings()
     {
+        $realInputArray = array(
+            'canvas' => array('name' => 'my-awesome-numbered-img-123',
+                              'height' => 50,
+                              'width'  => 50,
+                              'background-color' => 'green',
+                              'comments' => 'IT IS YOUR BIRTHDAY, IMAGE.'),  
+            'images' => array(
+                            array('url'  => 'bluebox.png',
+                              'top'  => 0,
+                              'left' => 0),
+                            array('url'  => 'redbox.png',
+                              'top'  => 0,
+                              'left' => 0)));
+
+        $fakeCr = new AbsolutelyCoolStub;
+
         
     }
 }
