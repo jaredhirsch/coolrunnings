e728d4eb0808f608eb5bf69f8b8094872a247878
Jared Hirsch <spambait@jaredhirsch.com>
2009-07-12 00:46:41 -0400

    crude but working: index.php goes from JSON (requesting images via localhost URL) all the way to generating a sprite and returning JSON path to the sprite.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'FrontController.php';
+require_once 'AbsolutelyCool.php';
+// I am reasonably sure this is how it works:
+
+    // given a front controller
+    
+    $fc = new FrontController;
+    
+    // start by inserting an absolutelyCool
+    // instance into the front controller
+    
+    $ac = new AbsolutelyCool;
+    // I guess we have to set AbsolutelyCool path separately
+    $ac->setSavePath('/var/www/html/');
+
+    $fc->setAbsolutelyCool($ac);
+
+    // set some other things we'll need
+    $fc->setWebRoot('/var/www/html/');
+    $fc->setRootUrl('http://www.example.com/');
+
+
+    // now: get request and funnel to AC to generate sprite
+    // ac returns path where sprite was saved
+    $expectedAsJson = '{"canvas":{"name":"my-awesome-numbered-img-123","height":50,"width":50,"background-color":"green","comments":"IT IS YOUR BIRTHDAY, IMAGE."},"images":[{"url":"http://localhost/coolRunnings/bluebox.png","top":0,"left":0},{"url":"http://localhost/coolRunnings/redbox.png","top":0,"left":0}]}';
+    $requestAsArray = $fc->decodeRequest($expectedAsJson);
+    $localSpritePath = $fc->dispatch($requestAsArray);
+
+    // replace local with web path, stuff into array, 
+    // convert into json, and emit!
+    $webPathAsArray = $fc->constructResponse($localSpritePath);
+    $webPathAsJson = $fc->responseAsJson($webPathAsArray);
+    //echo 'foo';
+    $fc->sendResponse($webPathAsJson);
