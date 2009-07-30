7826476e78bc40195030cc94a19ea0abd9821121
Jared Hirsch <spambait@jaredhirsch.com>
2009-07-29 21:44:54 -0400

    the beastly 'bootstrap' continues to grow. (1) had to add ob_start because of smush.it response causing headers to be sent too early. (2) random directory to save output images is actually generated and created here. (3) added extremely clumsy json/image split depending on 1querystring. need to filter this properly. Also need to replace the JSON parser.

--- library/Bootstrap.php
+++ library/Bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+/* add to mercurial asap. output buffering due to smush.it. */
+ob_start();
+
 require_once 'FrontController.php';
 require_once 'AbsolutelyCool.php';
 require_once 'Smusher.php';
@@ -13,13 +16,22 @@ require_once 'Smusher.php';
     
     $ac = new AbsolutelyCool;
     // I guess we have to set AbsolutelyCool path separately
-    $ac->setSavePath('/var/www/html/coolrunnings/public_images/');
+
+// here we introduce the random directory.
+$random = rand();
+$hashed = md5($random);
+$shortened = substr($hashed, 1, 10);
+$savePath = dirname(dirname(__FILE__)) . '/public_images/' . $shortened;
+mkdir($savePath);
+
+    //$ac->setSavePath(dirname(dirname(__FILE__)) . '/public_images/' . $shortened . '/');
+    $ac->setSavePath($savePath . '/');
 
     $fc->setAbsolutelyCool($ac);
 
     // set some other things we'll need
-    $fc->setWebRoot('/var/www/html/');
-    $fc->setRootUrl('http://localhost/');
+    $fc->setWebRoot('/var/www/html/jaredhirsch/');
+    $fc->setRootUrl('http://jaredhirsch.com/');
 
 
     // now: get request and funnel to AC to generate sprite
@@ -31,13 +43,40 @@ require_once 'Smusher.php';
     $webPathAsArray = $fc->constructResponse($localSpritePath);
 
 	// next, send to smush.it
-	$smush = new Smusher;
-	$smush->smush($webPathAsArray['url']);
-	if ($smush->isSmushed()) {
-		$webPathAsArray['url'] = $smush->getSmushedUrl();
-	}
+ 	$smush = new Smusher;
+ 	$smush->smush($webPathAsArray['url']);
+ 	if ($smush->isSmushed()) {
+ 		$webPathAsArray['url'] = $smush->getSmushedUrl();
+ 	}
 
+if ($_GET['format'] == 'json') {
     // convert into json, and emit!
     $webPathAsJson = $fc->responseAsJson($webPathAsArray);
-    //echo 'foo';
+	// trash the buffer
+    ob_end_clean();
     $fc->sendResponse($webPathAsJson);
+} elseif ($_GET['format'] == 'image') {
+	// trash the buffer
+    ob_end_clean();
+        // make an image out of our URL
+    if (!isset($webPathAsArray['url'])) {
+        die('error, no url generated. please play again.');
+    }
+    try {
+            // get local copy of image and save
+            // over the earlier one.
+        $localLocation = $ac->getLocalCopyOfImage($webPathAsArray['url'],
+                                                  $localSpritePath);
+            // imagick needs local files. hence this whole song and dance.
+        $imz = new Imagick($localSpritePath);
+    } catch (Exception $e) {
+        die('error, image url inaccessible. err msg was ' . 
+            $e->getMessage() .
+            '. thanks, please play again.');
+    }
+
+    // at this point, we have a real live image.
+    // so display it
+    header("Content-Type: image/png");
+    echo $imz;
+}
