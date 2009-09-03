ea31c895ba07699ef5346989c06927000d8a7b40
Jared Hirsch <spambait@jaredhirsch.com>
2009-09-03 01:37:49 -0500

    now successfully checking that input file exists

--- library/PngCrush.php
+++ library/PngCrush.php
@@ -4,9 +4,19 @@ class PngCrush
 {
     public function crush($inputFileName, $outputFileName) 
     {
+        $this->checkFileExists($inputFileName);
+
         // don't want the output, thx
         ob_start();
         passthru("pngcrush -force $inputFileName $outputFileName");
         $pngcrushResults = ob_get_clean();
+    }
+
+    private function checkFileExists($file)
+    {
+        $fileInfo = new SplFileInfo($file);
+        if (!$fileInfo->isFile()) {
+            throw new RuntimeException("file $file does not exist");
+        }
     }
 }
