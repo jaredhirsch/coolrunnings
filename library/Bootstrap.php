7f9ac632c3ab8641941f8ca32fec26fc8434e8c9
Jared Hirsch <spambait@jaredhirsch.com>
2009-09-07 18:09:28 -0700

    moved bootstrap code into a HUGE static class method

--- library/Bootstrap.php
+++ library/Bootstrap.php
@@ -4,95 +4,106 @@ ob_start();
 
 require_once 'FrontController.php';
 require_once 'AbsolutelyCool.php';
-// I am reasonably sure this is how it works:
 
-    // given a front controller
-    
-    $fc = new FrontController;
-    
-/*****************************************
- *
- * start by inserting an absolutelyCool
- * instance into the front controller
- *
- */
-    $ac = new AbsolutelyCool;
+class Bootstrap
+{
 
-    // I guess we have to set AbsolutelyCool path separately.
-    // here we introduce the random directory.
-    $random = rand();
-    $hashed = md5($random);
-    $shortened = substr($hashed, 1, 10);
-    $savePath = dirname(dirname(__FILE__)) . '/public_images/' . $shortened;
-    mkdir($savePath);
-    //$ac->setSavePath(dirname(dirname(__FILE__)) . '/public_images/' . $shortened . '/');
-    $ac->setSavePath($savePath . '/');
+    public static function run()
+    {
 
-// Front Controller config and stuff
+        // I am reasonably sure this is how it works:
 
-    $fc->setAbsolutelyCool($ac);
+            // given a front controller
+            
+            $fc = new FrontController;
+            
+        /*****************************************
+         *
+         * start by inserting an absolutelyCool
+         * instance into the front controller
+         *
+         */
+            $ac = new AbsolutelyCool;
 
-    $fc->setWebRoot('/var/www/html/');
-    $fc->setRootUrl('http://localhost/');
+            // I guess we have to set AbsolutelyCool path separately.
+            // here we introduce the random directory.
+            $random = rand();
+            $hashed = md5($random);
+            $shortened = substr($hashed, 1, 10);
+            $savePath = dirname(dirname(__FILE__)) . '/public_images/' . $shortened;
+            mkdir($savePath);
+            //$ac->setSavePath(dirname(dirname(__FILE__)) . '/public_images/' . $shortened . '/');
+            $ac->setSavePath($savePath . '/');
+
+        // Front Controller config and stuff
+
+            $fc->setAbsolutelyCool($ac);
+
+            $fc->setWebRoot('/var/www/html/');
+            $fc->setRootUrl('http://localhost/');
 
 
-// Finally we get to the part where 
-// the front controller is translating
-// HTTP input into a form ac can understand
+        // Finally we get to the part where 
+        // the front controller is translating
+        // HTTP input into a form ac can understand
 
-// all these steps should be pushed from
-// bootstrap into some kind of FC "dispatch" method
+        // all these steps should be pushed from
+        // bootstrap into some kind of FC "dispatch" method
 
-    // now: get request and funnel to AC to generate sprite
-    // ac returns path where sprite was saved
-    $requestAsArray = $fc->decodeRequest($_GET['absolute']);
-    $localSpritePath = $fc->dispatch($requestAsArray);
+            // now: get request and funnel to AC to generate sprite
+            // ac returns path where sprite was saved
+            $requestAsArray = $fc->decodeRequest($_GET['absolute']);
+            $localSpritePath = $fc->dispatch($requestAsArray);
 
-    // now inserting pngcrush wrapper
-    require_once 'PngCrush.php';
-    $crusher = new PngCrush;
-    try {
-        $crusher->crush($localSpritePath, $localSpritePath . ".crushed");
-        // if crushing succeeds, overwrite original file
-        copy($localSpritePath. ".crushed", $localSpritePath);
-        unlink($localSpritePath. ".crushed");
-    } catch (Exception $e) {}
+            // now inserting pngcrush wrapper
+            require_once 'PngCrush.php';
+            $crusher = new PngCrush;
+            try {
+                $crusher->crush($localSpritePath, $localSpritePath . ".crushed");
+                // if crushing succeeds, overwrite original file
+                copy($localSpritePath. ".crushed", $localSpritePath);
+                unlink($localSpritePath. ".crushed");
+            } catch (Exception $e) {}
 
-    // replace local with web path, stuff into array, 
-    $webPathAsArray = $fc->constructResponse($localSpritePath);
+            // replace local with web path, stuff into array, 
+            $webPathAsArray = $fc->constructResponse($localSpritePath);
 
-// here FC should decide what to do
-// based on the format of the response.
-// This should be pushed into FC.
+        // here FC should decide what to do
+        // based on the format of the response.
+        // This should be pushed into FC.
 
-    if ($_GET['format'] == 'json') {
-        // convert into json, and emit!
-        $webPathAsJson = $fc->responseAsJson($webPathAsArray);
-        // trash the buffer
-        ob_end_clean();
-        $fc->sendResponse($webPathAsJson);
-    } elseif ($_GET['format'] == 'image') {
-        // trash the buffer
-        ob_end_clean();
-            // make an image out of our URL
-        if (!isset($webPathAsArray['url'])) {
-            die('error, no url generated. please play again.');
-        }
-        try {
-                // get local copy of image and save
-                // over the earlier one.
-            $localLocation = $ac->getLocalCopyOfImage($webPathAsArray['url'],
-                                                      $localSpritePath);
-                // imagick needs local files. hence this whole song and dance.
-            $imz = new Imagick($localSpritePath);
-        } catch (Exception $e) {
-            die('error, image url inaccessible. err msg was ' . 
-                $e->getMessage() .
-                '. thanks, please play again.');
-        }
+            if ($_GET['format'] == 'json') {
+                // convert into json, and emit!
+                $webPathAsJson = $fc->responseAsJson($webPathAsArray);
+                // trash the buffer
+                ob_end_clean();
+                $fc->sendResponse($webPathAsJson);
+            } elseif ($_GET['format'] == 'image') {
+                // trash the buffer
+                ob_end_clean();
+                    // make an image out of our URL
+                if (!isset($webPathAsArray['url'])) {
+                    die('error, no url generated. please play again.');
+                }
+                try {
+                        // get local copy of image and save
+                        // over the earlier one.
+                    $localLocation = $ac->getLocalCopyOfImage($webPathAsArray['url'],
+                                                              $localSpritePath);
+                        // imagick needs local files. hence this whole song and dance.
+                    $imz = new Imagick($localSpritePath);
+                } catch (Exception $e) {
+                    die('error, image url inaccessible. err msg was ' . 
+                        $e->getMessage() .
+                        '. thanks, please play again.');
+                }
 
-        // at this point, we have a real live image.
-        // so display it
-        header("Content-Type: image/png");
-        echo $imz;
+                // at this point, we have a real live image.
+                // so display it
+                header("Content-Type: image/png");
+                echo $imz;
+            }
     }
+}
+
+Bootstrap::run();
