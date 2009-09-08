0d31abf91c08a514d20e74656a38a05d89a2ea08
Jared Hirsch <spambait@jaredhirsch.com>
2009-09-07 18:00:18 -0700

    starting to refactor Bootstrap away: moving json/image emitting logic into frontController

--- library/FrontController.php
+++ library/FrontController.php
@@ -33,6 +33,52 @@ class FrontController
         return array('url' => $filteredResponse);
     }
 
+    public function newConstructResponse($responsePath)
+    {
+        $filteredResponse = str_replace($this->webRootDirectory,
+                                    $this->rootUrl,
+                                    $responsePath);
+        return array('url' => $filteredResponse);
+
+    // snipped from Bootstrap
+
+        // here FC should decide what to do
+        // based on the format of the response.
+        // This should be pushed into FC.
+
+        if ($_GET['format'] == 'json') {
+            // convert into json, and emit!
+            $webPathAsJson = $fc->responseAsJson($webPathAsArray);
+            // trash the buffer
+            ob_end_clean();
+            $fc->sendResponse($webPathAsJson);
+        } elseif ($_GET['format'] == 'image') {
+            // trash the buffer
+            ob_end_clean();
+                // make an image out of our URL
+            if (!isset($webPathAsArray['url'])) {
+                die('error, no url generated. please play again.');
+            }
+            try {
+                    // get local copy of image and save
+                    // over the earlier one.
+                $localLocation = $ac->getLocalCopyOfImage($webPathAsArray['url'],
+                                                          $localSpritePath);
+                    // imagick needs local files. hence this whole song and dance.
+                $imz = new Imagick($localSpritePath);
+            } catch (Exception $e) {
+                die('error, image url inaccessible. err msg was ' . 
+                    $e->getMessage() .
+                    '. thanks, please play again.');
+            }
+
+            // at this point, we have a real live image.
+            // so display it
+            header("Content-Type: image/png");
+            echo $imz;
+        }
+    }
+
     protected $absolutelyCool;
 
     public function setAbsolutelyCool($aCoolObject)
