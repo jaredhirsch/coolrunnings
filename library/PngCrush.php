5d6a5e54a59db3d6150958367f493e1b3c4e9cb0
Jared Hirsch <spambait@jaredhirsch.com>
2009-09-11 22:35:46 -0700

    Pngcrush: removing 'brute' option to improve response time.

--- library/PngCrush.php
+++ library/PngCrush.php
@@ -10,7 +10,8 @@ class PngCrush
         // don't want the output, thx
         ob_start();
         // thx to Stoyan
-        passthru("pngcrush -rem alla -brute -reduce $inputFileName $outputFileName");
+        // brute is too slow passthru("pngcrush -rem alla -brute -reduce $inputFileName $outputFileName");
+        passthru("pngcrush -rem alla -reduce $inputFileName $outputFileName");
         $pngcrushResults = ob_get_clean();
     }
 
